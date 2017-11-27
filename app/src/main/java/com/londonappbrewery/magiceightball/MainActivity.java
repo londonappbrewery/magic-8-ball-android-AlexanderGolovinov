0c3d40120da152a593dc2bc989c25c539f414bc0
AlexanderGolovinov <alexgolovinoff@gmail.com>
2017-11-27 09:04:57 +0200

    8 ball_answer. Do not trust technology in that questions)

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,15 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnAsk = (Button) findViewById(R.id.btnAsk);
+
+        btnAsk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView displayText = (TextView) findViewById(R.id.textAsk);
+
+                EditText questionInput = (EditText) findViewById(R.id.questionText);
+                String question = questionInput.getText().toString();
+
+                displayText.setText(question);
+
+                Random random = new Random();
+                int randomNumber = random.nextInt(5);
+                int[] imageArray = {R.drawable.ball1,
+                                        R.drawable.ball2,
+                                        R.drawable.ball3,
+                                        R.drawable.ball4,
+                                        R.drawable.ball5};
+                ImageView imgDisplay = (ImageView) findViewById(R.id.imgAnswer);
+                imgDisplay.setImageResource(imageArray[randomNumber]);
+
+
+
+            }
+        });
+
+
+
+
+
+
+
     }
 }
